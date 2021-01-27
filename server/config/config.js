860d4c85b013ac6c6aa587307f9ba46f9023f500
CcdnDev <78099165+CcdnDev@users.noreply.github.com>
2021-01-27 13:55:37 -0300

    Update config.js
    Agregando las credenciales de Gmail de prueba para envios de Correos

--- server/config/config.js
+++ server/config/config.js
@@ -10,5 +10,5 @@ process.env.PORT = process.env.PORT || 5000;
 //========credenciales
 //========
 
-process.env.MAIL = process.env.MAIL;
-process.env.PASS = process.env.PASS;
+process.env.MAIL = 'mloyolaccdn@gmail.com';
+process.env.PASS = 'gnoeloebhiuivkok';
