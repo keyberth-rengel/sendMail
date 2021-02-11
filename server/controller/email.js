3705c48d1c559fcc75e8ef5c9e887bf50d5ad8a4
keyberth rengel <keyrengel24@gmail.com>
2021-02-11 07:22:01 -0500

    se agrego una plantilla para el body del msj

--- server/controller/email.js
+++ server/controller/email.js
@@ -1,52 +1,77 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 //======envio de email
 exports.emailSend = async (req, res) => {
-    const body = req.body;
-    if(!body){
-        return res.status(400).json({
-            ok:false,
-            message: 'Por favor llene todos los campos del formulario'
-        })
-    }
-    let transporter = nodemailer.createTransport({
-        service: "gmail", 
-        auth: {
-            user: process.env.MAIL, 
-            pass: process.env.PASS 
-        }
+  const body = req.body;
+  if (!body) {
+    return res.status(400).json({
+      ok: false,
+      message: "Por favor llene todos los campos del formulario",
     });
+  }
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.MAIL,
+      pass: process.env.PASS,
+    },
+  });
 
-    let mensaje = `
-    Nombre: ${body.nombre}
-    
-    Mail: ${body.correo}
-    
-    Asunto: ${body.asunto}
-    
-    ${body.contenido}
-    `;
+  let mensaje = "";
+  const {
+    user_name,
+    email_user,
+    hora_reservada,
+    fecha_reservada,
+    typeMsg,
+  } = body;
 
-    let opcionEmail = {
-        from: body.nombre || body.correo, 
-        to: body.correo, 
-        subject: body.asunto || 'Importante', 
-        text: mensaje    
+  if (typeMsg === "taller") {
+    const { nombre_taller, link_taller } = body;
+
+    mensaje = `
+    Taller inscrito exitosamente
+
+    Estimado ${user_name}
+  
+    Se ha inscrito al taller ${nombre_taller}. Para participar del taller debe ingresar por este link ${link_taller} el día ${fecha_reservada} a las ${hora_reservada}.
+  
+    En caso de cancelar se avisará por correo.
+  
+    Ccdn App`;
+  } else {
+    const { nombre_doctor } = body;
+    mensaje = `
+    Hora agendada exitosamente
+    
+    Estimado ${user_name}
+    
+    Se ha agendado una hora con ${nombre_doctor}, el día ${fecha_reservada} a las ${hora_reservada}.
+    
+    En caso de cancelar la hora se avisará con antelación.
+    
+    Ccdn App`;
+  }
+
+  let opcionEmail = {
+    from: process.env.MAIL,
+    to: email_user,
+    subject: body.asunto || "Importante",
+    text: mensaje,
+  };
+
+  await transporter.sendMail(opcionEmail, (error, mail) => {
+    if (error) {
+      return res.status(400).json({
+        ok: false,
+        error,
+        message: "No se envio el mensaje, Intentelo nuevamente",
+      });
     }
 
-    await transporter.sendMail(opcionEmail, (error, mail) => {
-        if (error) {
-            return res.status(400).json({
-                ok:false,
-                error,
-                message: 'No se envio el mensaje, Intentelo nuevamente'
-            })
-        }
-
-        res.json({
-            ok: true,
-            message: 'Mensaje enviado correctamente'
-        })
-    })
-}
-
+    res.json({
+      ok: true,
+      message: "Mensaje enviado correctamente",
+    });
+  });
+};
